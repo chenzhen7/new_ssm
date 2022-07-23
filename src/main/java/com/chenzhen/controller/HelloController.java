5051ef47abc1c7580d4e22a25ac7ea2571af9edd
chenzhen <1583296383@qq.com>
2022-07-23 17:59:32 +0800

    hot-fix commit

--- src/main/java/com/chenzhen/controller/HelloController.java
+++ src/main/java/com/chenzhen/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HelloController {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//hot-fix
 
     }
 
